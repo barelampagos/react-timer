6aba843769188992d50851c757c4642d97b01201
barelampagos <barelampagos@gmail.com>
2019-12-31 15:42:59 -0800

    Created CountdownForm component && passes input to Countdown component

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
 			Navigation: 'app/components/Navigation.jsx',
 			Countdown: 'app/components/Countdown.jsx',
 			Timer: 'app/components/Timer.jsx',
-			Clock: 'app/components/Clock.jsx'
+			Clock: 'app/components/Clock.jsx',
+			CountdownForm: 'app/components/CountdownForm.jsx'
 		},
 		// List of file extensions we should be able to process
 		extensions: ['', '.js', '.jsx']
