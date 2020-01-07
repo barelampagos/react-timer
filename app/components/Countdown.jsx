c0293f9497958d4336516b86de7365574a780c2f
barelampagos <barelampagos@gmail.com>
2020-01-06 19:22:20 -0800

    Fixing 0 second action

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -10,6 +10,7 @@ var Countdown = React.createClass({
 			countdownStatus: 'stopped'
 		};
 	},
+	// Called AFTER change to application (props + state)
 	componentDidUpdate: function(prevProps, prevState) {
 		if (this.state.countdownStatus !== prevState.countdownStatus) {
 			switch (this.state.countdownStatus) {
@@ -27,6 +28,11 @@ var Countdown = React.createClass({
 			}
 		}
 	},
+	// Called when component is 'unrendered' from view
+	componentWillUnmount: function() {
+		clearInterval(this.timer);
+		this.timer = undefined;
+	},
 	startTimer: function() {
 		// Remove 1 from count every second
 		this.timer = setInterval(() => {
@@ -34,6 +40,12 @@ var Countdown = React.createClass({
 			this.setState({
 				count: newCount >= 0 ? newCount : 0
 			});
+
+			if (newCount == 0) {
+				this.setState({
+					countdownStatus: 'stopped'
+				});
+			}
 		}, 1000);
 	},
 	handleSetCountdown: function(seconds) {
